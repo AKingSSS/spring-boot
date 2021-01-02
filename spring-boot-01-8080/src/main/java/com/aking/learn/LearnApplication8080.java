8d56f2cdfd709467a734951b82d714d9688ee5da
yangkang <2276306206@qq.com>
2021-01-02 15:44:54 +0800

    Initial commit

---
+++ spring-boot-01-8080/src/main/java/com/aking/learn/LearnApplication8080.java
@@ -0,0 +1,13 @@
+package com.aking.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnApplication8080 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnApplication8080.class, args);
+    }
+
+}
