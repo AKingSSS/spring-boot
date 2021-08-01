399d7c0fa9ab3ebc2880ba9e4ada60968d063db1
yangkang <yangkang@huohua.cn>
2021-08-01 23:10:51 +0800

    git commit --amend 小修改

---
+++ spring-boot-21-git/src/main/java/com/aking/learn/pojo/User.java
@@ -0,0 +1,13 @@
+package com.aking.learn.pojo;
+
+import java.io.Serializable;
+
+/**
+ * @author yangkang
+ * @date 2021/8/1
+ */
+public class User implements Serializable {
+    private static final long serialVersionUID = 4389327374727292295L;
+
+    private String name;
+}
